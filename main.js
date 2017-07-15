2970cf7f1994b844cd51355a2679e62d51fe875e
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-15 11:36:28 -0400

    Connected buttons on html to click events on js.

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+// Button Event Listeners
+ $("#newGame").click(function(){
+     console.log("New Game Button Clicked.");
+ });
+
+
+ $("#hit").click(function(){
+     console.log("Hit Button Clicked.");
+ });
+
+ 
+ $("#stand").click(function(){
+     console.log("Stand Button Clicked.");
+ });

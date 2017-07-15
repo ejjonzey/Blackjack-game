12153b2812df1dd7e5f1c461684a9a956bb3b370
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-15 12:40:10 -0400

    player hit function.

--- main.js
+++ main.js
@@ -1,44 +1,71 @@
-$(function() {
-	player1Hand = [];
-	dealerHand = [];
+$(function(){
+// Might not need a controller
+// var blackJackController = {
+// 	playerCardsValue: 0,
+// 	dealerCardsValue: 0,
+// 	dealerTurn: false,
+
+// }
+
+// Need to build card values and link images and store them
+
+var playerHand = [];
+var dealerHand = [];
+// var playerHandValue = [];
+// var dealerHandValue = [];
 
 
-//create Deck
-var gameData = {
-		deck: [],
-		buildDeck: function() {
-			var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10 J', '10 Q', '10 K', '11 A'];
-			var suits = ['hearts','diamonds', 'clubs', 'spades'];
-			for (var v = 0; v < values.length; v++) {
-				for(var s = 0; s < suits.length; s++) {
-					gameData.deck.push ({
-						name: parseInt(values[v]),
-						suits: suits[s],
-						player: null,
-						image: null,
-					});
-			}
-		}
-		console.log(gameData.deck);
+// Deck Setup
+var GameData = {
+	deck: [],
+	buildDeck: function() {
+		var names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10 J', '10 Q', '10 K', '11 A'];
+		var suits = ['Hearts','Diamonds','Spades','Clubs'];
+	    
+	    for( var n = 0; n < names.length; n++ ) {
+	        for( var s = 0; s < suits.length; s++ ) {
+	            // this.deck.push(names[n] + " " + suits[s]);
+	            GameData.deck.push({
+	            	name: parseInt(names[n]),
+	            	suit: suits[s],
+	            	player: null,
+	            	image: null,
+	            });
+	        }
+	    }
+
+	    console.log(GameData.deck);
+
+	   
 	}
-};
-gameData.buildDeck();
-
-function dealPlayerHand() {
-	console.log("dealPlayerHand is working.");
-	for(var i = 0; i <= 1; i++);
-		playerHand.push(gameData.deck[Math.floor(Math.random() * gameData.length)]);
-};
-console.log(playerHand);
-
-function dealDealerHand() {
-	console.log("dealer hand is working");
-	for (var i = 0; i <= 1; i++) {
-		dealerHand.push(gameData.deck[Math.floor(Math.random() * gameData.length)]);
-	}
-		console.log(dealerHand);
-
 }
+GameData.buildDeck();
+
+
+
+// Deal a new hand
+function dealNewPlayerHand() {
+	console.log('dealNewPlayerHand function is working');
+	for (var i = 0; i <= 1; i++) {
+		playerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
+		// playerHandValue.push(parseInt(playerHand[i]));
+	}
+	console.log(playerHand);
+	console.log("these are the value of the cards in playerHand")
+	
+}
+
+function dealNewDealerHand() {
+	console.log("dealNewDealerHand function is working");
+	for (var i = 0; i <= 1; i++) {
+		dealerHand.push(GameData.deck[Math.floor(Math.random() * GameData.deck.length)]);
+		// dealerHandValue.push(parseInt(dealerHand[i]));
+	}
+	console.log(dealerHand);
+	console.log("these are the value of the cards in dealerHand")
+	
+}
+
 
 
 function playerHandValue(array) {

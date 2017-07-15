11187d1138557118c3f5722c7faea8e5678012d0
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-15 12:25:40 -0400

    added player hand value function

--- main.js
+++ main.js
@@ -6,24 +6,39 @@ $(function() {
 //create Deck
 var gameData = {
 		deck: [],
-		buildDeck = function() {
+		buildDeck: function() {
 			var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10 J', '10 Q', '10 K', '11 A'];
 			var suits = ['hearts','diamonds', 'clubs', 'spades'];
 			for (var v = 0; v < values.length; v++) {
-				for(car s = 0; s < suits.length; s++) {
-					gameData.deck.push {
+				for(var s = 0; s < suits.length; s++) {
+					gameData.deck.push ({
 						name: parseInt(values[v]),
 						suits: suits[s],
 						player: null,
 						image: null,
-					};
+					});
 			}
 		}
 		console.log(gameData.deck);
-	};
+	}
 };
 gameData.buildDeck();
 
+function dealPlayerHand() {
+	console.log("dealPlayerHand is working.");
+	for(var i = 0; i <= 1; i++);
+		playerHand.push(gameData.deck[Math.floor(Math.random() * gameData.length)]);
+};
+
+function playerhandValue(array) {
+	console.log("playerHandValue working");
+	var player1 = 0;
+	for(var i = 0; i < playerHandValue.length; i++) {
+		player1 = plyer1 + array[i];
+	}
+}
+
+console.log(playerHand);
 // Button Event Listeners
  $("#newGame").click(function(){
      console.log("New Game Button Clicked.");
